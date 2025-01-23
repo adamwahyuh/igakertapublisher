2a2d5cb9693bc749f0e2f882728717bc9c5ff393
adamwahyuh <adamwahyu875@gmail.com>
2025-01-23 08:14:43 +0700

    Updating Index, Services, Blog

--- asset/js/components/footer.js
+++ asset/js/components/footer.js
@@ -1,29 +1,51 @@
 class Navbar extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
-        <footer class="text-white pt-4">
-            <div class="container">
+        <footer class="bg-purple text-white py-5">
+            <div class="container-fluid">
                 <div class="row">
-                <div class="col-md-4">
-                    <h2>Tentang kami</h2>
-                    <p>Menyajikan beragam bacaan berkualitas untuk memperkaya pengetahuan dan imajinasi Anda.</p>
-                </div>
-                <div class="col-md-4">
-                    <h5>Quick Links</h5>
-                    <ul class="list-unstyled">
-                    <li><a href="index.html" class="text-white">Home</a></li>
-                    <li><a href="services.html" class="text-white">Services</a></li>
-                    <li><a href="contact.html" class="text-white">Contact</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h5>Kontak kami</h5>
-                    <p>Email: igakertapublisher@example.com</p>
-                    <p>Phone: +62 81234567891</p>
-                </div>
-                </div>
-                <div class="text-center mt-3">
-                <p>&copy; 2025 igakertapublisher. All rights reserved.</p>
+                    <!-- Informasi -->
+                    <div class="col-md-2">
+                        <h5 class="fw-bold">INFORMASI</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white text-decoration-none">Tentang Kami</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2">
+                        <h5 class="fw-bold">BANTUAN</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white text-decoration-none">Kontak Kami</a></li>
+                            <li><a href="#" class="text-white text-decoration-none">F.A.Q</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2">
+                        <h5 class="fw-bold">KATEGORI BUKU</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white text-decoration-none">Buku Ajar</a></li>
+                            <li><a href="#" class="text-white text-decoration-none">Buku Monograf</a></li>
+                            <li><a href="#" class="text-white text-decoration-none">Buku Referensi</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <h5 class="fw-bold">TENTANG KAMI</h5>
+                        <p>IGAKERTA BOOK Catalog menjual berbagai macam kategori buku dari komik hingga novel fiksi, serta buku ilmiah lain dengan harga yang bersaing dan kualitas terjamin.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h5 class="fw-bold">KONTAK</h5>
+                        <p>
+                            Alamat: Jalan Palem Raya no 15 RT 05/RW 08<br>
+                            Petukangan Utara Pesanggrahan Jakarta Selatan<br>
+                            DKI Jakarta 12260<br>
+                            No. HP: +62 857-8276-3529<br>
+                            Email: <a href="mailto:igakertapublisher@gmail.com" class="text-white text-decoration-none">igakertapublisher@gmail.com</a>
+                        </p>
+                        <div>
+                            <a href="#" class="text-white me-3"><i class="bi bi-twitter"></i></a>
+                            <a href="#" class="text-white me-3"><i class="bi bi-facebook"></i></a>
+                            <a href="#" class="text-white me-3"><i class="bi bi-instagram"></i></a>
+                            <a href="#" class="text-white me-3"><i class="bi bi-google"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
